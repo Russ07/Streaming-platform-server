f13430c35f86d5ed75eefb2390cdda9f332e0eef
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-08-11 14:51:42 +0100

    created a function to write videos. Fixed a path on both function

--- routes/videos.js
+++ routes/videos.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
-// Function to read trees
+// Function to read videos
 function readVideos() {
-    const videosFile = fs.readFileSync('./data/trees.json');
+    const videosFile = fs.readFileSync("../data/videos.json");
     const videosData = JSON.parse(videosFile);
     return videosData;
+}
+
+// Function to write videos
+function writeVideos(data) {
+    const stringifiedData = JSON.stringify(data);
+    fs.writeFileSync("../data/videos.json", stringifiedData);
 }
