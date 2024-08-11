8461aff70ccb4ecc5a9c916c2d4b6865cf3d7ba7
Russ07 <90725405+Russ07@users.noreply.github.com>
2024-08-11 16:06:21 +0100

    created requets that gets videoLIst data

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 8080;
-const videosRouts = require("./routes/videos")
+const videosRouts = require("./routes/videos");
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 app.use("/", videosRouts);
 
+app.use("/images", express.static("./public/images"));
+
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
+});
+app.get('/test', (req, res) => {
+    res.send("please work");
 });
--- routes/videos.js
+++ routes/videos.js
@@ -1,16 +1,44 @@
 const express = require('express');
-const app = express();
 const fs = require('fs');
+const router = express.Router();
 
 // Function to read videos
 function readVideos() {
-    const videosFile = fs.readFileSync("../data/videos.json");
+    const videosFile = fs.readFileSync("./data/videos.json");
     const videosData = JSON.parse(videosFile);
     return videosData;
 }
 
 // Function to write videos
+
 function writeVideos(data) {
     const stringifiedData = JSON.stringify(data);
-    fs.writeFileSync("../data/videos.json", stringifiedData);
+    fs.writeFileSync("./data/videos.json", stringifiedData);
 }
+
+// GET /videoList
+router.get('/videos', (req, res) => {
+
+    // 1. Read the videos data (and parse it)
+    const videosData = readVideos();
+
+    // 2. Strip down the videos data
+    const strippedData = videosData.map((video) => {
+        return {
+            id: video.id,
+            title: video.title,
+            channel: video.channel,
+            image: video.image,
+        }; 
+    });
+
+    // 3. Respond with that data
+    res.json(strippedData);
+    
+});
+
+
+
+
+
+module.exports = router;
